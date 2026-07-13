d574e5258f6f0ed686163f7aa435c8ca81897dd2
P-C-M-P <soragabelti@gmail.com>
2026-07-13 14:27:15 +0000

    Add code to main.c

---
+++ C-programming/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    // data types and format specifiers
+    int x=4;
+    float y=4.56734;
+    double e=2.718;
+    char z= '!';
+    char name[] = "NOT BROTATO";
+
+    printf("%d", x);
+
+    printf("%f", y);
+
+    printf("%lf", e);
+
+    printf("%c", z);
+
+    printf("%s", name);
+
+    return 0;
+
+}
