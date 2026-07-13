57d7f1f325d0560c0d61673632362e40a4d4c74c
P-C-M-P <soragabelti@gmail.com>
2026-07-13 14:33:50 +0000

    ADD comment and precison specifier

--- C-programming/main.c
+++ C-programming/main.c
@@ -4,16 +4,17 @@
 int main(){
     // data types and format specifiers
     // width specified by adding desired width between percentage sign and letter if format specifier
+    // precision by adding decimal places
 
     int x=4;
     float y=4.56734;
-    double e=2.718;
+    double e=2.71828;
     char z= '!';
     char name[] = "NOT BROTATO";
 
     printf("%3d", x);
 
-    printf("%f", y);
+    printf("%.6f", y);
 
     printf("%lf", e);
 
