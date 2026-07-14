4c670db49750a38ab6b86c7af556ab60156ef347
P-C-M-P <soragabelti@gmail.com>
2026-07-14 15:41:39 +0000

    ADD code on switches in file if-statements.c

--- C-programming/if-statements.c
+++ C-programming/if-statements.c
@@ -3,6 +3,7 @@
 int main(){
      
     int age;
+    int DOW;
 
     printf("How old are you? ");
     scanf("%d", &age);
@@ -19,6 +20,39 @@ int main(){
         printf("You're not allowed to drink.\n");
 
     }
+ 
+    printf("What day of the week is it(1-7)? ");
+    scanf("%d", &DOW);
+
+    switch(DOW){
+
+        case 1:
+            printf("it is monday");
+            break;
+        
+        case 2:
+            printf("it is tuesday");
+            break;
+        case 3:
+            printf("it is wednesday");
+            break;
+        case 4:
+            printf("it is thursday");
+            break;
+        case 5:
+            printf("it is friday");
+            break;
+        case 6:
+            printf("it is saturday");
+            break;
+        case 7:
+            printf("it is sunday");
+            break;
+        default:
+            printf("invalid choice");
+
+    }
+
 
     return 0;
 
