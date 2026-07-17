640436dc3a040a1d7ef48abc808abd184e30a3dd
P-C-M-P <soragabelti@gmail.com>
2026-07-17 14:17:19 +0000

    MERGE files while-loops.c and for-loops.c into file loops.c (kinda)

--- C-programming/loops.c
+++ C-programming/loops.c
@@ -16,4 +16,26 @@ int main(){
         printf("%d", timer)
     }
 
+
+    int age;
+    int age1 = 0;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    while(age <= 0){  // checks the condition first before proceeding with the block of code 
+        printf("Please enter your real age: ");
+        scanf("%d", &age);
+    }
+    printf("You are %d years old!", age);
+
+
+    do{  // does the block of code first then enters the loop after checking the condition
+        printf("Please enter your real age: ");
+        scanf("%d", &age1);
+    }while(age1 <= 0);
+
+    printf("You are %d years old!", age1);
+
 }
+
