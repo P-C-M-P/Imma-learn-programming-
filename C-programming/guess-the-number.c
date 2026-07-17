6094e5bcd7a0ac38bb2321677cea0b22c5cbb1ff
P-C-M-P <soragabelti@gmail.com>
2026-07-17 15:45:15 +0000

    ADD extra code to file guess-the-number.c

--- C-programming/guess-the-number.c
+++ C-programming/guess-the-number.c
@@ -1,3 +1,6 @@
+/*
+THIS IS ONE WAY TO DO IT, BUT THE OTHER WAY WRITTEN BELOW IS JUST BETTER
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
@@ -30,4 +33,36 @@ int main(){
     printf("That's exactly it!\n");
     printf("It took you %d tries", tries);
 
+}*/
+
+// THE AFOREMENTIONED BETTER WAY
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+
+    srand(time(NULL));
+    
+    int guess;
+    int tries=0;
+    int max = 100, min = 1, random;
+    random = (rand() % (max - min + 1)) + min;
+    
+    do{
+        printf("Enter your guess(1-100): ");
+        scanf("%d", &guess);
+        tries++;
+        if(guess<random){
+            printf("Too small!\n");
+        }
+        else if(guess>random){
+            printf("Too large!\n");
+        }
+        
+    }while(guess!=random);
+    
+    printf("That's exactly it!\n");
+    printf("It took you %d tries", tries);
 }
