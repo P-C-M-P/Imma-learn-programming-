6fae222da690ec28e797d340885b0ef2e97618b2
P-C-M-P <soragabelti@gmail.com>
2026-07-17 14:24:58 +0000

    ADD code on continue keyword

--- C-programming/loops.c
+++ C-programming/loops.c
@@ -5,20 +5,18 @@
 int main(){
 
     int timer;
+    int age;
+    int age1 = 0;
     
     printf("Enter a timer in seconds: ");
     scanf("%d", &timer);
 
     for( ; timer >= 0; timer--){
-      /*^btw you add initialization here but if your varaiable is initialized elsewhere then no need to add it here*/
+      /*^btw you add initialization here but if your varaiable is initialized elsewhere then no need to add it here, as in line 38*/
         Sleep(1000);  // a delay of 1000ms between each turn of the loop(upper case S in Sleep, used in windows)
         sleep(1000); // a delay of 1000ms between each turn of the loop(lower case s in sleep, used in mac and linux)
-        printf("%d", timer)
+        printf("%d", timer);
     }
-
-
-    int age;
-    int age1 = 0;
 
     printf("Enter your age: ");
     scanf("%d", &age);
@@ -36,6 +34,15 @@ int main(){
     }while(age1 <= 0);
 
     printf("You are %d years old!", age1);
+
+    for(int x = 10; x>=0; x--){
+        
+        if(x == 6){
+            continue; // so basically this line says when x equals 6, skip the line which says print the number, so the output shouldn't print the number 6
+        }
+        
+        printf("%d\n", x);
+    }
 
 }
 
