0050f660d954377fd8f9149c139fc7a382d47ff8
P-C-M-P <soragabelti@gmail.com>
2026-07-18 15:52:11 +0000

    ADD code to file arrays.c(here, i didn't clean up the code so just carefully read it, i mean it is easy to understand)

--- C-programming/arrays.c
+++ C-programming/arrays.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 
-//ARRAYS ARE FIXED COLLECTIONS OF DATA, KIND OF LIKE VARIABLES BUT THEY CAN STORE MORE THAN A SINGLE VALUE
-
 int main(){
 
-    int numbers[] = {1, 2, 3, 5, 7, 9, 7346634, 890, 68, 342,345345};
+    int numbers[5] = {0};
  //   printf("%d\n", numbers[0]);  // prints out 1, since 1 is located at the index of zero in the array numbers
 
     for(int i = 0; i < 8; i++){
         printf("%d\n", numbers[i]);
     }
 
-  int size = sizeof(numbers)/sizeof(numbers[0]); // computes the number of elements in an array by using the formula
-                                                // the sizeof function equates how many bytes are allocated to the data type. 
-                                               // the formula is then just evaluating the number of elements in the array by dividing the total bytes allocated to the bytes allocated to a single element
-  for(int i = 0; i < size; i++){                                             
+    int size = sizeof(numbers)/sizeof(numbers[0]);
+  
+    for(int i = 0; i < size; i++){
       printf("%d ", numbers[i]);
-  }
+    }
 
+    for(int x = 0; x < 5; x++){
+        printf("Enter a number for my array: ");
+        scanf("%d", &numbers[x]);
+    }
+
+    for(int i = 0; i < 5; i++){
+        printf("%d ", numbers[i]);
+    }
+    
 }
