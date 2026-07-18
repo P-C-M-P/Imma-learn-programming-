a7c7b2de006171f0cbbaca6761d21a77acf860d4
P-C-M-P <soragabelti@gmail.com>
2026-07-18 14:18:27 +0000

    FIX code in file rock-paper-scissors.c(according to brocode)

--- C-programming/rock-paper-scissors-shoot.c
+++ C-programming/rock-paper-scissors-shoot.c
@@ -2,18 +2,86 @@
 #include<stdlib.h>
 #include<time.h>
 
+int getcomputerchoice();
+
+int getuserchoice();
+
+void compare(int userchoice, int computerchoice);
+
 int main(){
-
+    
     srand(time(NULL));
-    int random;
-    int min = 1, max = 3;
+    
+    int userchoice = getuserchoice();
+    int computerchoice = getcomputerchoice();
+    
+    switch(userchoice){
+        case 1:
+            printf("You chose ROCK!\n");
+            break;
+        case 2:
+            printf("You chose PAPER!\n");
+            break;
+        case 3:
+            printf("You chose SCISSORS!\n");
+            break;
+    }
+    
+    switch(computerchoice){
+        case 1:
+            printf("Computer chose ROCK!\n");
+            break;
+        case 2:
+            printf("Computer chose PAPER!\n");
+            break;
+        case 3:
+            printf("Computer chose SCISSORS!\n");
+            break;
+    }
+    
+    compare(userchoice, computerchoice);
+    
+    return 0;
+}
 
-    random = (rand() % (max - min + 1)) + min;
-    printf("Enter a choice!\n");
+int getcomputerchoice(){
+    
+    return ((rand() % 3) + 1);
+    
+}
 
-    printf("1. ROCK");
-    printf("2. PAPER");
-    printf("1. SCISSORS");
+int getuserchoice(){
+    
+    int choice;
+    
+    do{
+        printf("1. ROCK\n");
+        printf("2. PAPER\n");
+        printf("3. SCISSORS\n");
+        printf("Enter a choice: ");
+        scanf("%d", &choice);
+    }while(choice < 1 || choice > 3);
+    
+    return choice;
+    
+}
 
-    switch()
+void compare(int userchoice, int computerchoice){
+    
+    if(userchoice == computerchoice){
+        printf("It's a tie");
+    }
+    else if(userchoice == 1 && computerchoice == 3){
+        printf("You win!");
+    }
+    else if(userchoice == 2 && computerchoice == 1){
+        printf("You win!");
+    }
+    else if(userchoice == 3 && computerchoice == 2){
+        printf("You win!");
+    }
+    else{
+        printf("YOU LOSE!!!!!!!");
+    }
+    
 }
