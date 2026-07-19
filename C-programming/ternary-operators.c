ee5699bec140c4496d3cc5b66d0a1edbed8dcd3f
P-C-M-P <soragabelti@gmail.com>
2026-07-19 14:51:50 +0000

    ADD extra code to file ternary-operators.c regarding the typedef keyword

--- C-programming/ternary-operators.c
+++ C-programming/ternary-operators.c
@@ -1,23 +1,38 @@
 #include<stdio.h>
 
+typedef int num; // defined the predefined type of ints as being the same as nums
+                 // basically it's like renaming the type int into nums
+                 
+typedef char string[300];
+
 int main(){
 
-    int x, y;
+    num x, y;
     
     printf("ENTER TWO NUMBERS: ");
     scanf("%d %d", &x,&y);
 
     
-    int k = x>y? 1:2;  
+    num k = x>y? 1:2;  
     // basically says store the value of 1 into k if x is larger than y, and 2 if y is larger than x
-    
+
     switch(k){
         case 1:
-            printf("x is larger!");
+            printf("x is larger!\n");
             break;
         case 2:
-            printf("y is larger!");
+            printf("y is larger!\n");
             break;
     }
+
+    num l = 4;
+    num m = 45;
+    num n = l + m;
+    
+    string name = "dave";
+    
+    printf("%s\n", name);
+
+    printf("%d", n);
 
 }
