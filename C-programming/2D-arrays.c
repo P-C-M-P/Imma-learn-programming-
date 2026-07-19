9641201d714b64801ed81fc6cb3d3442364ff913
P-C-M-P <soragabelti@gmail.com>
2026-07-19 13:47:23 +0000

    ADD some extra code on file 2D-arrays.c

--- C-programming/2D-arrays.c
+++ C-programming/2D-arrays.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
 
-
-// 2D arrays are arrays of arrays so: {{}, {}, {}}, kinda stuff you get it?
-
-
 int main(){
 
     char numbers[][3] = {{'1','2','3'},
@@ -11,6 +7,22 @@ int main(){
                       {'7','8','9'},
                       {'*','0','#'}
     };
+    
+    char fruits[][10] = {"GREEN", 
+                         "BANANA", 
+                         "TOMATO", 
+                         "POTATO", 
+                         "LAPUTA"};
+                         
+    fruits[0][0] = 'N';
+    fruits[0][5] = 'G';
+    
+    int x = sizeof(fruits)/sizeof(fruits[0]);
+    
+    for(int i = 0; i < x; i++){
+        printf("%s ", fruits[i]);
+    }
+    printf("\n");
 
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 3; j++){
@@ -19,4 +31,21 @@ int main(){
         printf("\n");
     }
 
+	char names[3][25];
+	char name[4][26];
+	
+	int size = sizeof(name)/sizeof(name[0]);
+
+    
+    for(int i = 0; i < size; i++){
+        printf("Enter a name: ");
+        fgets(name[i], sizeof(name[i]), stdin);
+        name[i][sizeof(name[i]) - 1] = '\0';
+    }
+    
+    
+    for(int i = 0; i < size; i++){
+        printf("%s", name[i]);
+    }
 }
+
