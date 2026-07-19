fcb9f1fa7fd28893bcacaac007c0923f33a9627b
P-C-M-P <soragabelti@gmail.com>
2026-07-19 16:00:58 +0000

    ADD extra code to file structs.c(please read this code carefully and study ti as well)

--- C-programming/structs.c
+++ C-programming/structs.c
@@ -9,6 +9,8 @@ typedef struct{
     bool isfulltime;
 }Student;
 
+void printstudentinfo(Student student);
+
 int main(){
 
     Student student1 = {89, "spongebob", 3.45, false};
@@ -18,19 +20,33 @@ int main(){
                            //so at least set it to 0 as is shown above
                     
     
-    strcpy(student4.name, "greg");
+    strcpy(student4.name, "greg"); // so basically this assign the name greg to the student4.name value, which i end up overriding in lines 36-38
     
     
     printf("How old are you? ");
     scanf("%d", &student4.age);
     
-
-    printf("%d\n", student1.age);
-    printf("%s\n", student1.isfulltime ? "yea":"nah");
-    printf("%.2f\n", student1.gpa);
-    printf("%s\n", student1.name);
+    printf("What's your gpa? ");
+    scanf("%f", &student4.gpa);
     
-    printf("%s\n", student4.name);
-    printf("%d\n", student4.age);
-    printf("%.2f\n", student4.gpa);
+    printf("are you a full time student? ");
+    scanf("%d", &student4.isfulltime);
+    getchar();
+    
+    printf("what's your name? ");
+    fgets(student4.name, sizeof(student4.name), stdin);
+    student4.name[strlen(student4.name)-1] = '\0';
+    
+    printstudentinfo(student1);
+    printstudentinfo(student2);
+    printstudentinfo(student3);
+    printstudentinfo(student4);
+}
+
+void printstudentinfo(Student student){
+    printf("%d\n", student.age);
+    printf("%s\n", student.isfulltime ? "yea":"nah");
+    printf("%.2f\n", student.gpa);
+    printf("%s\n", student.name);
+    printf("\n");
 }
