a3bce982caa334816a59f33f5f2b597e1a01f986
P-C-M-P <soragabelti@gmail.com>
2026-07-21 16:01:11 +0000

    ADD file calloc.c (please read more on the calloc and malloc functions, cause i don't get them at all)

--- C-programming/calloc.c
+++ C-programming/calloc.c
@@ -1,5 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
     
+    int number = 0;
+    printf("how many numbers do you want? ");
+    scanf("%d", &number);
+    
+    int *scores = calloc(number, sizeof(int)); /*so basically calloc is malloc but like it intially sets all values to zero i guess
+                                                 so a malloc function may intially give you garbage values, but with calloc you intially set things to be zero first*/
+    
+    if(scores == NULL){
+        printf("ERROR!");
+    }
+    
+    for(int i = 0; i < number; i++){
+        printf("Enter a number#%d: ", i + 1);
+        scanf("%d", &scores[i]);
+    }
+    
+    for(int i = 0; i < number; i++){
+        printf("%d ", scores[i]);
+    }
+
+    free(scores);
+    scores = NULL;
 }
