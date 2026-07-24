880cb8e3e9582d96cbb6ac6d63e49122d6fc60f4
P-C-M-P <soragabelti@gmail.com>
2026-07-24 13:24:44 +0000

    ADD extra code to file check-largest-number.c(an alternative solution to the problem)

--- C-programming/check-largest-number.c
+++ C-programming/check-largest-number.c
@@ -1,5 +1,6 @@
 
 // so either is a valid solution although they kinda suck overall, do fix them if you'd like
+// the third option is the better solution
 
 
 /*#include<stdio.h>
@@ -53,3 +54,26 @@ int main(){
     
     printf("the largest number is %d", current_largest);
 }
+
+/*#include<stdio.h>
+
+int main(){
+    
+    int current_largest = 0;
+    int num;
+    
+    int i = 0;
+
+    do{
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        
+        if(i == 0 || num > current_largest){
+            current_largest = num;
+        }
+        i++;
+    }while(i < 4);
+    
+    printf("the largest number is %d", current_largest);
+}
+*/
